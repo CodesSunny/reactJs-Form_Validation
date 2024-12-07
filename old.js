9426bbafdbd4a1892d992f9e5e979c780da50409
TechSunny <sunnymotivation2@gmail.com>
2024-12-07 19:00:41 +0530

    completed validation

--- old.js
+++ old.js
@@ -1,124 +1,187 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
-import './App.css'
-import { useState } from 'react';
+import { Formik } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const dark = "bg-gray-500";
+  const light = "bg-white";
+  const [theme, setTheme] = useState(dark);
 
-  const dark='bg-gray-500';
-  const light='bg-white';
-
-  const [error,setError] = useState("");
-  const [formData,setformData]= useState({
-    uname:"",  //set initial key:value
-    email:"",
-    mobile:"",
-    password:"",
-    cpw:""
-  })
-
-  const [theme,setTheme] = useState(dark);
-
-  const changeTheme=()=>{
-    if(theme ==dark){
-      setTheme(light)
-    }else{
-      setTheme(dark)
-    }  
-  }
-
-
-  const validateForm=(e)=>{
-   e.preventDefault();
-    if(error){
-      alert("all fields are mandatory")
-    }else{
-      alert("Form submitted successfully")
-    }  
-   }
-  
-  const validateInput=(e)=>{
-    e.target.value === ""; //initial state
-    const input = e.target;
-    const value= input.value;
-    const key=input.name;
-    if(value.trim() === ""){
-      setError("field can't be empty")
-    }else{
-      setformData({
-        ...formData,  //prev value
-        [key]: value   //updated value
-        })
-      setError("")   //remove error
+  // define dark/light mode
+  const changeTheme = () => {
+    if (theme == dark) {
+      setTheme(light);
+    } else {
+      setTheme(dark);
     }
-
-  }
-
-
-
-
+  };
+  
   return (
     <>
-    <div className={`w-full h-[100%] ${theme} flex flex-col items-center justify-center rounded overflowX-hidden p-6 relative`}
-     >
-        <FontAwesomeIcon icon={faCircleHalfStroke}
+      <div
+        className={`w-full h-[100vh] ${theme} flex flex-col items-center justify-center rounded overflow-hidden relative `}
+      >
+        <FontAwesomeIcon
+          icon={faCircleHalfStroke}
           onClick={changeTheme}
-         className='text-2xl absolute top-1 right-2 '
-         />
-      <h1 className='min-w-[550px] py-4  font-bold text-white text-2xl text-center bg-gray-600 border-gray-400 border-b-4 border-b-2'>Form Validation </h1>
-      <form
-         onSubmit={(e)=>validateForm(e)}
-         action="#" className='min-w-[550px] bg-gray-600 py-6 flex flex-col items-center gap-4'>
-        <p className='text-white'>{JSON.stringify(formData)}</p>
-        <div className='flex flex-col gap-2 w-96 '>
-          <label htmlFor="name" className='text-lg'>Name:</label>
-          <input
-           onChange={(e)=>validateInput(e)} 
-           name='uname' value={formData.uname}
-           type="text" id='name'placeholder='enter ur name here' className='py-2 px-4 rounded'/>
-          <span className='text-red-500'> {error}</span>
-        </div>
+          className="text-2xl absolute top-1 right-2 "
+        />
+        <h1 className="min-w-[550px] py-4  font-bold text-white text-2xl text-center bg-gray-600 border-gray-400 border-b-4 border-b-2">
+          Form Validation{" "}
+        </h1>
+        <Formik
+          initialValues={{name:'', email: '', mobile:"", password: '', cpw:"" }}
+          validate={values => {
+          const errors = {};
+          if(!values.name){
+            errors.name="required";
+          }else if(values.name.length<2){
+            errors.name="name should be at least 2 characters long";
+          }
+          else if (!values.email) {
+            errors.email = 'Required';
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }else if (!values.mobile) {
+            errors.mobile  = 'Required';
+          }else if(values.mobile.length<10){
+            errors.mobile ="should be 10 digits";
+          }else if (!values.password) {
+            errors.password  = 'Required';
+          }else if (values.password.length<6) {
+            errors.password  = 'at least 6 characters';
+          }else if (!values.cpw) {
+            errors.cpw  = 'Required';
+          }else if (values.cpw !== values.password) {
+            errors.cpw  = 'must be same as password';
+          }
+          return errors;
+      }}
 
-        <div className='flex flex-col gap-2 w-96 '>
-          <label htmlFor="email" className='text-lg'>E-mail:</label>
-          <input
-             onChange={(e)=>validateInput(e)}
-             name='email' value={formData.email}
-             type="email" id='email'placeholder='abc@mail.com' className='py-2 px-4 rounded'/>
-          <span className='text-red-500'> {error}</span>
-        </div>
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form
+            onSubmit={handleSubmit}
+            action="#"
+            className="min-w-[550px] bg-gray-600 py-6 flex flex-col items-center gap-4"
+          >
+            <div className="flex flex-col gap-2 w-96 ">
+              <label htmlFor="name" className="text-lg">
+                Name:
+              </label>
+              <input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                name="name"
+                type="text"
+                id="name"
+                placeholder="enter ur name here"
+                className="py-2 px-4 rounded"
+              />
+              <span className="text-red-500">
+              {errors.name && touched.name && errors.name}
+              </span>
+            </div>
 
-        <div className='flex flex-col gap-2 w-96 '>
-          <label htmlFor="mobile" className='text-lg'>Mobile No:</label>
-          <input
-            onChange={(e)=>validateInput(e)}
-            name='mobile' value={formData.mobile}
-            type="mobile" id='mobile'placeholder='enter ur mobile no here' minLength={10} maxLength={10} className='py-2 px-4 rounded'/>
-          <span className='text-red-500'> {error}</span>
-        </div>
-        <div className='flex flex-col gap-2 w-96 '>
-          <label htmlFor="password" className='text-lg'>Password:</label>
-          <input
-            onChange={(e)=>validateInput(e)}
-            name ="password" value={formData.password}
-            type="password" id='password' placeholder='*****' minLength={5} className='py-2 px-4 rounded'/>
-          <span className='text-red-500'> {error}</span>
-        </div>
+            <div className="flex flex-col gap-2 w-96 ">
+              <label htmlFor="email" className="text-lg">E-mail:</label>
+              <input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                name="email"
+                type="email"
+                id="email"
+                placeholder="abc@mail.com"
+                className="py-2 px-4 rounded"
+              />
+              <span className="text-red-500">
+              {errors.email && touched.email && errors.email}
+              </span>
+            </div>
 
-        <div className='flex flex-col gap-2 w-96 '>
-          <label htmlFor="cpw" className='text-lg'>Confirm Password:</label>
-          <input
-            onChange={(e)=>validateInput(e)}
-            name='cpw' value={formData.cpw}
-            type="text" id='cpw' placeholder='enter same password' className='py-2 px-4 rounded'/>
-          <span className='text-red-500'> {error}</span>
-        </div>
+            <div className="flex flex-col gap-2 w-96 ">
+              <label htmlFor="mobile" className="text-lg"> Mobile No:</label>
+              <input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.mobile}
+                name="mobile"
+                type="mobile"
+                id="mobile"
+                placeholder="enter ur mobile no here"
+                maxLength={10}
+                className="py-2 px-4 rounded"
+              />
+              <span className="text-red-500">
+              {errors.mobile && touched.mobile && errors.mobile}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2 w-96 ">
+              <label htmlFor="password" className="text-lg"> Password:</label>
+              <input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                name="password"
+                type="password"
+                id="password"
+                placeholder="***"
+                className="py-2 px-4 rounded"
+              />
+              <span className="text-red-500">
+              {errors.password && touched.password && errors.password}
+              </span>
+            </div>
 
-        <button type="submit" className='bg-gray-500 px-12 py-4 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'>Submit </button>
-      </form>
-    </div>
+            <div className="flex flex-col gap-2 w-96 ">
+              <label htmlFor="cpw" className="text-lg"> Confirm Password:</label>
+              <input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.cpw}
+                name="cpw"
+                type="text"
+                id="cpw"
+                placeholder="enter same password"
+                className="py-2 px-4 rounded"
+              />
+              <span className="text-red-500">
+              {errors.cpw && touched.cpw && errors.cpw}
+              </span>
+            </div>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="bg-gray-500 px-12 py-4 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95"
+            >
+              Submit{" "}
+            </button>
+        </form>
+      )}
+    </Formik>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,219 +1,129 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
-import "./App.css";
-import { useState } from "react";
+import { object, string,ref } from 'yup';
+import { useFormik } from 'formik';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import './App.css'
+import { useState } from 'react';
+
+// define validation schema
+let userSchema = object({
+  uname: string().min(2).required("required"),
+  email: string().email().required("required"),
+  mobile:string().matches(/^[0-9]+$/, "Only numbers are allowed")
+  .min(10, "Mobile number must be exactly 10 digits")
+  .max(10, "Mobile number must be exactly 10 digits")
+  .required("Mobile number is required"),
+  password:string().min(6).required("required"),
+  cpw:string().required("required").oneOf([ref("password"), null], "Passwords must match")
+});
+
+// define initial input values
+const initialValues={
+  uname:"",
+  email:"",
+  mobile:"",
+  password:"",
+  cpw:""
+}
 
 function App() {
-  const dark = "bg-gray-500";
-  const light = "bg-white";
-  const defaultFields = {
-    uname: "",
-    email: "",
-    mobile: "",
-    password: "",
-    cpw: "",
-  };
 
-  const [formData, setFormData] = useState(defaultFields);
-  const [formError, setFormError] = useState(defaultFields);
-
-  const formErrorConditions = {
-    required: {
-      uname: "Name is required",
-      email: "Email is required",
-      mobile: "Mobile is required",
-      password: "Password is required",
-      cpw: "Confirm Password is required",
-    },
-    minLength: {
-      password: {
-        value: 6,
-        message: "Password must be 6 characters long",
-      },
-    },
-    pattern: {
-      email: {
-        regex: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-        message: "Invalid email address",
-      },
-      mobile: {
-        regex: /^[0-9]{10}$/,
-        message: "Invalid mobile number",
-      },
-    },
-  };
-
-  const [theme, setTheme] = useState(dark);
+  const dark='bg-gray-500';
+  const light='bg-white';
+  const [theme,setTheme] = useState(dark);
 
   // define dark/light mode
-  const changeTheme = () => {
-    if (theme == dark) {
-      setTheme(light);
-    } else {
-      setTheme(dark);
-    }
-  };
+  const changeTheme=()=>{
+    if(theme ==dark){
+      setTheme(light)
+    }else{
+      setTheme(dark)
+    }  
+  }
 
-  const validateField = (key, value) => {
-    return new Promise((resolve, reject) => {
-      if (key === "cpw") {
-        if (value !== formData.password) {
-          reject("Password does not match");
-        } else resolve();
-      } else {
-        if (formErrorConditions.required[key] && !value) {
-          reject(formErrorConditions.required[key]);
-        } else if (
-          formErrorConditions.minLength[key] &&
-          value.trim().length < formErrorConditions.minLength[key].value
-        ) {
-          reject(formErrorConditions.minLength[key].message);
-        } else if (
-          formErrorConditions.pattern[key] &&
-          !formErrorConditions.pattern[key].regex.test(value)
-        ) {
-          reject(formErrorConditions.pattern[key].message);
-        } else {
-          resolve();
-        }
-      }
-    });
-  };
-
-  const handleChange = async (e) => {
-    const { name, value } = e.target; //e.target returns input object
-    try {
-      setFormData({ ...formData, [name]: value });
-      await validateField(name, value);
-      setFormError((prev) => {
-        delete prev[name];
-        return { ...prev };
-      });
-    } catch (error) {
-      setFormError({ ...formError, [name]: error });
+  // define formik function & dstructure methods
+  const {values,handleChange,handleBlur,touched,handleSubmit,errors}=useFormik({
+    initialValues:initialValues,
+    validationSchema:userSchema,
+    onSubmit:(values,action)=>{  //stores input values
+     console.log(values);
+     action.resetForm(); //empty after submit
     }
-  };
+  })
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    if (Object.keys(formError).length === 0) {
-      alert("form submitted successfully");
-      setFormData(defaultFields);
-    } else {
-      alert("please validate form before submitting");
-    }
-  };
 
   return (
     <>
-      <div
-        className={`w-full h-[100vh] ${theme} flex flex-col items-center justify-center rounded overflow-hidden relative `}
-      >
-        <FontAwesomeIcon
-          icon={faCircleHalfStroke}
+    <div className='w-full h-[100vh] ${theme} flex flex-col items-center justify-center rounded overflow-hidden relative'>
+        <FontAwesomeIcon icon={faCircleHalfStroke}
           onClick={changeTheme}
-          className="text-2xl absolute top-1 right-2 "
-        />
-        <h1 className="min-w-[550px] py-4  font-bold text-white text-2xl text-center bg-gray-600 border-gray-400 border-b-4 border-b-2">
-          Form Validation{" "}
-        </h1>
-        {/* <p className='text-white'>{JSON.stringify(formData)}</p> */}
-        <form
-          onSubmit={submitForm}
-          action="#"
-          className="min-w-[550px] bg-gray-600 py-6 flex flex-col items-center gap-4"
-        >
-          <div className="flex flex-col gap-2 w-96 ">
-            <label htmlFor="name" className="text-lg">
-              Name:
-            </label>
-            <input
-              onChange={handleChange}
-              name="uname"
-              value={formData.uname}
-              type="text"
-              id="name"
-              placeholder="enter ur name here"
-              className="py-2 px-4 rounded"
-            />
-            <span className="text-red-500">{formError.uname}</span>
-          </div>
+          // onClick={()=>setTheme(theme == dark ? light : dark)}
+         className='text-2xl absolute top-1 right-2 '
+         />
+      <h1 className='min-w-[550px] py-4  font-bold text-white text-2xl text-center bg-gray-600 border-gray-400 border-b-4 border-b-2'>Form Validation </h1>
+      <form
+        onSubmit={handleSubmit}
+         action="#" className='min-w-[550px] bg-gray-600 py-6 flex flex-col items-center gap-4'>
+        <div className='flex flex-col gap-2 w-96 '>
+          <label htmlFor="name" className='text-lg'>Name:</label>
+          <input
+           onChange={handleChange}
+           onBlur={handleBlur}
+           value={values.uname}
+           name='uname'
+           type="text" id='name'placeholder='enter ur name here' className='py-2 px-4 rounded'
+           />    
+          <span className='text-red-500'>{touched.uname && errors.uname}</span>
+        </div>
 
-          <div className="flex flex-col gap-2 w-96 ">
-            <label htmlFor="email" className="text-lg">
-              E-mail:
-            </label>
-            <input
-              onChange={handleChange}
-              name="email"
-              value={formData.email}
-              type="email"
-              id="email"
-              placeholder="abc@mail.com"
-              className="py-2 px-4 rounded"
-            />
-            <span className="text-red-500">{formError.email}</span>
-          </div>
+        <div className='flex flex-col gap-2 w-96 '>
+          <label htmlFor="email" className='text-lg'>E-mail:</label>
+          <input
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+             name='email' 
+             type="email" id='email'placeholder='abc@mail.com' className='py-2 px-4 rounded'/>
+          <span className='text-red-500'> {touched.email && errors.email }</span>
+        </div>
 
-          <div className="flex flex-col gap-2 w-96 ">
-            <label htmlFor="mobile" className="text-lg">
-              Mobile No:
-            </label>
-            <input
-              onChange={handleChange}
-              name="mobile"
-              value={formData.mobile}
-              type="mobile"
-              id="mobile"
-              placeholder="enter ur mobile no here"
-              minLength={10}
-              maxLength={10}
-              className="py-2 px-4 rounded"
-            />
-            <span className="text-red-500">{formError.mobile}</span>
-          </div>
-          <div className="flex flex-col gap-2 w-96 ">
-            <label htmlFor="password" className="text-lg">
-              Password:
-            </label>
-            <input
-              onChange={handleChange}
-              name="password"
-              value={formData.password}
-              type="password"
-              id="password"
-              placeholder="***"
-              className="py-2 px-4 rounded"
-            />
-            <span className="text-red-500">{formError.password}</span>
-          </div>
+        <div className='flex flex-col gap-2 w-96 '>
+          <label htmlFor="mobile" className='text-lg'>Mobile No:</label>
+          <input
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.mobile}
+            name='mobile' 
+            type="tel" id='mobile' placeholder='enter ur mobile no here' maxLength={10} className='py-2 px-4 rounded'/>
+          <span className='text-red-500'>{touched.email && errors.mobile}</span>
+        </div>
+        <div className='flex flex-col gap-2 w-96 '>
+          <label htmlFor="password" className='text-lg'>Password:</label>
+          <input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+            name='password' 
+            type="password" id='password' placeholder='***' minLength={5} className='py-2 px-4 rounded'/>
+          <span className='text-red-500'>{touched.email && errors.password} </span>
+        </div>
 
-          <div className="flex flex-col gap-2 w-96 ">
-            <label htmlFor="cpw" className="text-lg">
-              Confirm Password:
-            </label>
-            <input
-              onChange={handleChange}
-              name="cpw"
-              value={formData.cpw}
-              type="text"
-              id="cpw"
-              placeholder="enter same password"
-              className="py-2 px-4 rounded"
-            />
-            <span className="text-red-500">{formError.cpw}</span>
-          </div>
-
-          <button
-            type="submit"
-            className="bg-gray-500 px-12 py-4 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95"
-          >
-            Submit{" "}
-          </button>
-        </form>
-      </div>
+        <div className='flex flex-col gap-2 w-96 '>
+          <label htmlFor="cpw" className='text-lg'>Confirm Password:</label>
+          <input
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.cpw}
+            name='cpw' 
+            type="text" id='cpw' placeholder='enter same password' className='py-2 px-4 rounded'/>
+          <span className='text-red-500'>{touched.email && errors.cpw } </span>
+        </div>
+                                                                                                                                          
+        <button type="submit" className='bg-gray-500 px-12 py-4 rounded-lg text-white text-xl font-semibold shadow-lg hover:cursor-pointer hover:scale-95'>Submit </button>
+      </form>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
